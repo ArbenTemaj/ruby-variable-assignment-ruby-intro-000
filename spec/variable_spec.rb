e27151efba7fef764331d981fedb340b6e1a2583
Arben Temaj <arbentemaj@gmail.com>
2020-01-14 18:52:30 +0000

    Automatically backed up by Learn

--- spec/variable_spec.rb
+++ spec/variable_spec.rb
@@ -3,8 +3,6 @@ describe "./variable.rb" do
 
   it "defined a local variable called greeting and set it equal to 'Hello World'" do
   (  greeting = "hello world",)
-
-    greeting = "Hello World"
   end
 
 end
